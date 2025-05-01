84e2925d81da39e0d304e6c71efeb23e1b013df0
Nezumi-2711 <phanhuuloi15@gmail.com>
2025-05-01 10:14:28 +0700

    feat: implement the search filter

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,16 +1,44 @@
 import { ReactNode } from 'react';
+import configPromise from '@payload-config';
+import { getPayload } from 'payload';
 
 import Navbar from './navbar';
 import Footer from './footer';
+import SearchFilters from './search-filters';
+import { Category } from '@/payload-types';
 
 interface LayoutProps {
 	children: ReactNode;
 }
 
-const Layout = ({ children }: LayoutProps) => {
+const Layout = async ({ children }: LayoutProps) => {
+	const payload = await getPayload({
+		config: configPromise,
+	});
+
+	const data = await payload.find({
+		collection: 'categories',
+		depth: 1,
+		where: {
+			parent: {
+				exists: false,
+			},
+		},
+	});
+
+	const formattedData = data.docs.map((category) => ({
+		...category,
+		subcategories: (category.subcategories?.docs ?? []).map((doc) => ({
+			...(doc as Category),
+			subcategories: undefined,
+		})),
+	}));
+
 	return (
 		<div className="flex flex-col min-h-screen">
 			<Navbar />
+
+			<SearchFilters data={formattedData} />
 
 			<div className="flex-1 bg-[#F4F4F0]">{children}</div>
 
