da7c3b26035a40b37a3fd1021b2c2155c5179753
Nezumi-2711 <phanhuuloi15@gmail.com>
2025-07-19 16:20:08 +0700

    feat: implement auth states

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -21,6 +21,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
+  cookiePrefix: 'funroad',
   collections: [Users, Media, Categories],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
